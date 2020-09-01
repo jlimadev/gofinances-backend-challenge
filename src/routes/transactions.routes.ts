14ad4f499e4e17d854c4b67c95d4eaa6ab9b84fb
jlimadev <jlima.dev@gmail.com>
2020-09-01 15:28:52 -0300

    added: getBalance implemented

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -9,8 +9,11 @@ const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
   const transactionsRepository = getCustomRepository(TransactionsRepository);
+
   const transactions = await transactionsRepository.find();
-  return response.json(transactions);
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
