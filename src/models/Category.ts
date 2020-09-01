c46517aab660c10a3a0c2c67d59d2f5e6a84a048
jlimadev <jlima.dev@gmail.com>
2020-09-01 11:29:51 -0300

    added: typeorm decorators to models

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,10 +1,23 @@
+import {
+  Column,
+  UpdateDateColumn,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
 class Category {
+  @PrimaryGeneratedColumn()
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
